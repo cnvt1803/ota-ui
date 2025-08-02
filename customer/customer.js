f361187c6f95f709f01c2fa8fa7cf4858fce95b0
cnvt1803 <vantruongasds@gmail.com>
2025-08-03 00:14:03 +0700

    up

--- customer/customer.js
+++ customer/customer.js
@@ -892,7 +892,7 @@ async function submitEditDeviceForm(event) {
     const loadingToast = createToast('Updating device...', 'info', 10000);
 
     // Call update API
-    const updateData = await updateDeviceInfor({
+    const updateData = await updateDeviceInfo({
       device_id: deviceId,
       name: deviceName,
       location: deviceLocation,
